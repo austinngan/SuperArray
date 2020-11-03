162a29cefb3419e0d002e56c48c0fe16de8dcd1c
austinngan <angan20@stuy.edu>
2020-11-02 19:28:42 -0500

    added get method

--- SuperArray.java
+++ SuperArray.java
@@ -2,6 +2,7 @@ public class SuperArray{
   private String[] data;
   private int size;
   public SuperArray(){
+    data=new String[10];
     size=0;
   }
   public int size(){
@@ -10,5 +11,13 @@ public class SuperArray{
   public boolean add(String element){
     data[size]=element;
     return true;
+  }
+  public String get(int index){
+    if ((size<index)||(index<0)){
+      return "No element in this index";
+    }
+    else{
+      return data[index];
+    }
   }
 }
