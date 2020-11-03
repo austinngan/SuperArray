d77f55cdeeb89639b56968bc616013655445b545
austinngan <angan20@stuy.edu>
2020-11-02 21:55:06 -0500

    Modified add method and fixed code for other methods

--- SuperArray.java
+++ SuperArray.java
@@ -9,34 +9,27 @@ public class SuperArray{
     return size;
   }
   public boolean add(String element){
+    if (size>=data.length){
+      resize();
+    }
     data[size]=element;
+    size=size+1;
     return true;
   }
   public String get(int index){
-    if ((index>=size)||(index<0)){
-      return "No element in this index";
-    }
-    else{
-      return data[index];
-    }
+    return data[index];
   }
   public String set(int index, String element){
-    if (index<size){
-      data[index]=element;
-    }
-    else{
-      return "No element in this index";
-    }
+    String old=data[index];
+    data[index]=element;
+    return old;
   }
   private void resize(){
     String[] larger=new String[data.length+1];
     for (int i=0;i<data.length;i++){
       larger[i]=data[i];
     }
-    String[] data=new String[larger.length];
-    for int(y=0;y<larger.length;y++){
-      data[y]=larger[y]
-    }
+    data=larger;
   }
 
 }
