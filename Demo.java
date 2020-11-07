ac5d7c723320892369440ba1cf9575b16ffb7602
austinngan <angan20@stuy.edu>
2020-11-07 15:04:15 -0500

    deleted code that would make the file not compile

--- Demo.java
+++ Demo.java
@@ -10,17 +10,12 @@ public class Demo{
   }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
-    SuperArray a=new SuperArray();
-    SuperArray b=new SuperArray();
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
-    a.add("9"); a.add("1"); a.add("2"); a.add("2"); a.add("3"); a.add("4");
-    b.add("0"); b.add("4"); b.add("2"); b.add("2"); b.add("9");
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
-    System.out.println(findOverlap(a, b));
   }
   public static SuperArray findOverlap(SuperArray a, SuperArray b){
     SuperArray c=new SuperArray();
